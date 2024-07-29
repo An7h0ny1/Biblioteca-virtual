92b86d1a7213a26b5c45dd7df2b1ad23a93377b4
An7h0ny1 <91570972+An7h0ny1@users.noreply.github.com>
2024-07-29 17:45:57 -0500

    menu page

--- book-network-ui/src/app/modules/book/book.module.ts
+++ book-network-ui/src/app/modules/book/book.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BookRoutingModule } from './book-routing.module';
+import { MainComponent } from './pages/main/main.component';
+import { MenuComponent } from "./components/menu/menu.component";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainComponent
+  ],
   imports: [
     CommonModule,
-    BookRoutingModule
-  ]
+    BookRoutingModule,
+    MenuComponent
+]
 })
 export class BookModule { }
